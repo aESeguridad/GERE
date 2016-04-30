53beedf8d963f406d22530dbf1ae301e00df73b9
aesculta <dranzeraef@gmail.com>
2016-04-30 14:05:35 +0200

    .

--- source/models/client.py
+++ source/models/client.py
@@ -1,19 +1,21 @@
 #/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from source.main import db
 
 class Client(db.Model):
     __tablename__ = 'client'
     
+    #: Column
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String, nullable = False)
     company = db.Column(db.String, nullable = True)
     vat = db.Column(db.String(11), nullable = False)
     
+    #: Relationship
+    contacts = db.relationship('Contact', backref='Client', lazy='dynamic')
+    
     def __init__(self, **kwargs):
-        pass
+        if kwargs:
+            self.name = kwargs['name']
     def __repr__(self):
-        pass
-    def getcontacts(self):
         pass
